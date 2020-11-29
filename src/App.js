dfeedb65cb9f49f6a18fd648ea445c19e3c1c7a3
yannrelave <74868273+yannrelave@users.noreply.github.com>
2020-11-29 09:57:40 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Liste de films</h1>
+      <div className="card"></div>
+    </div>
+  );
+}
